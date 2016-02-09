f2a57d045eee9cec69dcbd6ce7d51632db85144c
Mnshuster <shuster.mc@husky.neu.edu>
2016-02-09 18:40:48 -0500

    pepperoni moves randomly with array

--- pizza game/sketch.js
+++ pizza game/sketch.js
@@ -1,49 +1,69 @@
-var x = 500; 
-var y = 450; 
+var a = 500; 
+var b = 450; 
 var position=0;
 var speed = 3;
-var xpos = 0;
-var ypos = 0;
-
+var toppings = [];
 
 function setup () {
   createCanvas(1000,600);
- 
-
+  for(var i = 0; i < 50; i++){
+  toppings[i]= {
+    x: random (0,width),
+    y: random (-5000,0),
+    display: function() {
+      fill(255,0,0);
+      ellipse(this.x,this.y,25,25);
+    },
+    
+    move: function(){
+      this.y = this.y+3;
+     }
+   }
+  }
 }
 
-function pepperoni (x,y) {
+/* function pepperoni (x,y) {
   
+   
+    for (var y = 0; y <= height; y = y+random(0,500))
     for (var x = 0; x < width; x = x + 50) {
     fill(255,0,0);
     ellipse(x,y,25,25);
+    
+    
     }  
 }
+*/
 
 
 function draw () {
-  
   background(0);
-  fill(184,134,11);
+   fill(184,134,11);
   strokeWeight(0);
-  ellipse(x+50, y+50, 200,75); 
+  ellipse(a+50, b+50, 200,75); 
   fill(255,215,0);
-  ellipse(x+50, y+50, 170, 50);
-   
+  ellipse(a+50, b+50, 170, 50);
+  for (var i = 0; i < toppings.length; i++){
+    toppings[i].move();
+    toppings[i].display();
+  }
+  
+  
     if (keyIsPressed && keyCode === RIGHT_ARROW) {
-            x = x + 10;  
+            a = a + 10;  
     } 
     if (keyIsPressed && keyCode === LEFT_ARROW) {
-            x = x - 10;
+            a = a - 10;
     }  
     
     
-    pepperoni(200,position-30);
-    if (position > height || position < 0) {
-       speed = speed * -1;
-
+    /* pepperoni(200,position-30);
+    if (position > height) {
+      pepperoni(200,position-30);
+    
     } 
     
     position = position + speed ;
+    */
 }
  
